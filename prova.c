14465de071783c8d3b3f15f8c8c7984fb79249d0
Alessandro Pellegrini <alessandro@pellegrini.tk>
2014-11-30 23:21:16 +0100

    Reverting gitignore changes

---
+++ prova.c
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdlib>
+
+void antanizzata(char Alfio, char tarapia);
+
+void antanizzata(char Alfio, char tarapia) {
+    char vicesindaco = 101;
+    std::cout << (vicesindaco) << std::endl;
+    char pastene = vicesindaco + 7;
+    std::cout << (pastene) << std::endl;
+    std::cout << (pastene) << std::endl;
+    std::cout << (tarapia) << std::endl;
+    std::cout << (Alfio) << std::endl;
+    std::cout << (tarapia) << std::endl;
+    char scappellamento = 114;
+    std::cout << (scappellamento) << std::endl;
+    std::cout << (pastene) << std::endl;
+    char Antani = pastene - 8;
+    std::cout << (Antani) << std::endl;
+    return;
+}
+
+int main() {
+    int bucaiola = 0;
+    char prematurata = 72;
+    std::cout << (prematurata) << std::endl;
+    char antifurto = 87;
+    char cofandina = prematurata << 1;
+    cofandina = cofandina - 33;
+    antanizzata(antifurto, cofandina);
+    return bucaiola;
+}
